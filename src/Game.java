4c07d53e19c2463ce995c1d07a6000d9d9cc92ff
Grégory Clain <g.clain69@gmail.com>
2022-06-21 23:19:17 +0200

    add conditionnals

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,27 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
         Random r = new Random();
-        int justprix = r.nextInt(200);
-        int prix = 0;
-        System.out.println("Le justprix : " + justprix);
+        int tryNumber = 0;
+        int intervalMax = 20;
+        int justprix = r.nextInt(intervalMax);
+        int price = 0;
+        while(justprix != price) {
+            System.out.println("entrez un numéro entre 0 et " + intervalMax);
+            Scanner scan = new Scanner(System.in);
+
+            if(justprix > price){
+                System.out.println("C'est plus");
+            }else {
+                System.out.println("c'est moins");
+            }
+            price = scan.nextInt();
+            tryNumber++;
+
+        }
+        System.out.println("Bravo le justprix est : " + justprix);
+        System.out.println("Nombre d'essai : " + tryNumber);
     }
 }
