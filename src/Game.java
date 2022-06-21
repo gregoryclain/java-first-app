b126082ca86dee419ad75d99424c9f2a52899361
Grégory Clain <g.clain69@gmail.com>
2022-06-21 23:04:27 +0200

    test println

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class Game {
     public static void main(String[] args) {
-
+        Random r = new Random();
+        int justprix = r.nextInt(200);
+        int prix = 0;
+        System.out.println("Le justprix : " + justprix);
     }
 }
